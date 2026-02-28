874b93b950a5066835582cd15ca42b51756f47d7
Dwi Irawan <dwiirawan08@gmail.com>
2026-02-28 16:11:15 +0700

    add CORS

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"golang-clean-crud/routes"
 	"golang-clean-crud/service"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,6 +19,13 @@ func main() {
 	config.DB.AutoMigrate(&entity.User{})
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	// Dependency Injection
 	// repository
